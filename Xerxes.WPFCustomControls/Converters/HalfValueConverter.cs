3740d741a5abc9152c86b21610e6f4689cfac70e
andrewmccafferty <andymccafferty@gmail.com>
2013-09-08 15:45:34 +0100

    First draft of version using single selected element instead of changing style of selected element in treeview

---
+++ Xerxes.WPFCustomControls/Converters/HalfValueConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Xerxes.WPFCustomControls.Converters
+{
+    /// <summary>
+    /// Used to facilitate centring in canvases
+    /// http://blogorama.nerdworks.in/entry-CenteringelementsonacanvasinWP.aspx
+    /// </summary>
+    public class HalfValueConverter : IMultiValueConverter
+    {
+        #region IMultiValueConverter Members
+
+        public object Convert(object[] values,
+                              Type targetType,
+                              object parameter,
+                              CultureInfo culture)
+        {
+            if (values == null || values.Length < 2)
+            {
+                throw new ArgumentException(
+                    "HalfValueConverter expects 2 double values to be passed" +
+                    " in this order -> totalWidth, width",
+                    "values");
+            }
+
+            double totalWidth = (double)values[0];
+            double width = (double)values[1];
+            return (object)((totalWidth - width) / 2);
+        }
+
+        public object[] ConvertBack(object value,
+                                    Type[] targetTypes,
+                                    object parameter,
+                                    CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
